bc7437ac1bb2d949216e05b7c2f8743cae21fcb2
Xillez <kentwh@stud.ntnu.no>
2019-02-05 16:14:03 +0100

    COD-5 Added index.html, main.scss with proper styling and WebGLDetect.js for handling webgl error.

---
+++ frontend/js/WebGLDetect.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function() {
+    
+    var canvas = document.createElement("canvas");
+    canvas.style.visibility = "hidden";
+
+    var gl = canvas.getContext("webgl");
+    if (!gl)                                // No webgl context was found.
+    {
+        // Add notification bar as first element under body.
+        var notificationbar = document.createElement("div");
+        notificationbar.className = "notificationbar";
+        document.body.insertBefore(notificationbar, document.body.firstChild);
+        notificationbar.innerHTML = "You're browser doesn't support WebGL! Visit: <a href=\"https://get.webgl.org/\">WebGL support!</a>";
+        notificationbar.style.visibility = "visible";
+    }
+}, false);
