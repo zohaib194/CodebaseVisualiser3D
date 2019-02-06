c52ddc1a561626f68b2c2c17aefdb0269ceb8d36
Xillez <kentwh@stud.ntnu.no>
2019-02-06 16:30:57 +0100

    COD-5 Added some more comments and removed "document." form all dom functions used

--- frontend/js/WebGLDetect.js
+++ frontend/js/WebGLDetect.js
@@ -1,12 +1,14 @@
 window.addEventListener("load", function() {
-    var canvas = document.createElement("canvas");
+
+    // Add hidden canvas for webgl context.
+    var canvas = createElement("canvas");
     canvas.style.visibility = "hidden";
 
     var gl = canvas.getContext("webgl");
     if (!gl)                                // No webgl context was found.
     {
-        // Add notification bar as first element under body.
-        var notificationbar = document.createElement("div");
+        // Add notification bar as first element under body (very top of page).
+        var notificationbar = createElement("div");
         notificationbar.className = "notificationbar";
         document.body.insertBefore(notificationbar, document.body.firstChild);
         notificationbar.innerHTML = "You're browser doesn't support WebGL! " + 
