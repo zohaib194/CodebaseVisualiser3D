c1976fcc457c1192f9cb6126be38050528dccc4c
zohaib <zohaib_b13@hotmail.com>
2019-02-09 22:55:15 +0100

    COD-4 Fixed the missing whitespace from antlr output.

--- backend/parser/src/main/java/me/codvis3D/ast/CppListener.java
+++ backend/parser/src/main/java/me/codvis3D/ast/CppListener.java
@@ -3,8 +3,26 @@ package me.codvis.ast;
 import me.codvis.ast.parser.CPP14BaseListener;
 import me.codvis.ast.parser.CPP14Parser;
 
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.CharStream;
+
 public class CppListener extends CPP14BaseListener {
-    @Override public void enterFunctiondefinition(CPP14Parser.FunctiondefinitionContext ctx) {  //see gramBaseListener for allowed functions
-        System.out.println("Function name: " + ctx.getText());      //code that executes per rule
+
+    @Override 
+    public void enterFunctiondefinition(CPP14Parser.FunctiondefinitionContext ctx) {  
+        
+        // Get start and stop of function.
+        int a = ctx.start.getStartIndex();
+	    int b = ctx.stop.getStopIndex();
+
+	    // Get the input stream of function definition rule.
+	    Interval interval = new Interval(a,b);
+	    CharStream input = ctx.start.getInputStream();
+
+	    // Get the function contect and remove the definition.
+	    String funcName = input.getText(interval);
+	    funcName = funcName.substring(0, funcName.indexOf("{"));
+
+        System.out.println("Function name: " + funcName);      
     }
  }
