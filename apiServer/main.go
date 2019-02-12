9140ebb58ab0fdde79dec4414874d416ef733358
zohaib <zohaib_b13@hotmail.com>
2019-02-12 09:12:18 +0100

    COD-4 Added entrypoint to fetch simple functions from a specified project repository key

---
+++ apiServer/main.go
@@ -0,0 +1,54 @@
+//main deals with environment and api definition.
+package main
+
+import (
+	"github.com/zohaib194/CodebaseVisualizer3D/apiServer/controller"
+	"github.com/zohaib194/CodebaseVisualizer3D/apiServer/model"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Indicate the importance of a warning
+const (
+	logWarn  = "[WARNING]: "
+	logError = "[ERROR]: "
+	logInfo  = "[INFO]: "
+)
+
+func main() {
+
+	// Get environment variables
+	port := os.Getenv("PORT")
+	model.RepoPath = os.Getenv("REPOSITORY_PATH")
+	dbLocation := os.Getenv("DB_LOCATION")
+	model.JavaParserPath = os.Getenv("JAVA_PARSER")
+
+	// Validate variables
+	if len(port) == 0 {
+		log.Fatal(logError + "$PORT was not set")
+	}
+	if len(model.RepoPath) == 0 {
+		log.Fatal(logError + "$REPOSITORY_PATH was not set")
+	}
+	if len(dbLocation) == 0 {
+		log.Fatal(logError + "$DB_LOCATION was not set")
+	}
+	if len(model.JavaParserPath) == 0 {
+		log.Fatal(logError + "$JAVA_PARSER was not set")
+	}
+
+	// Variable setup
+	model.DB.DatabaseURL = dbLocation
+	if err := model.DB.Init(); err != nil {
+		log.Fatal(logError + "Could not initialize database")
+	}
+
+	// API routings
+	http.HandleFunc("/repo/add", controller.RepoController{}.NewRepoFromURI)
+	http.HandleFunc("/repo/", controller.RepoController{}.ParseSimpleFunc)
+
+	// Start server
+	log.Printf(logInfo+"Listening on port: %v", port)
+	http.ListenAndServe(":"+port, nil)
+}
---
+++ apiServer/model/mongodb.go
@@ -0,0 +1,132 @@
+package model
+
+import (
+	"errors"
+	"log"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// MongoDB stores the details of the DB connection.
+type MongoDB struct {
+	DatabaseURL  string
+	DatabaseName string
+	RepoColl     string
+}
+
+const logWarn = "[WARNING]: "
+const logError = "[ERROR]: "
+const logInfo = "[INFO]: "
+
+// DB is a mongo database with name CodeVis3D and collection gitRepository
+var DB = &MongoDB{"mongodb://localhost", "CodeVis3D", "gitRepository"}
+
+// Init - initializes the mongoDB database
+func (db *MongoDB) Init() error {
+	// Setup session with database
+	log.Println(logInfo + "Dialing database! Setting up a session!")
+	session, err := mgo.Dial(db.DatabaseURL)
+
+	// Check for session error
+	if err != nil {
+		log.Println(logError + "Failed dialing database")
+		return err
+	}
+
+	// Set up currency collation indexing
+	index := mgo.Index{
+		Key:        []string{"uri"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+
+	if err != nil {
+		log.Println(logError + "Failed to drop databse!")
+		return err
+	}
+
+	// Ensure collation follows the index
+	log.Println(logInfo + "Make collection: " + db.RepoColl + "Ensure \"index\"")
+	err = session.DB(db.DatabaseName).C(db.RepoColl).EnsureIndex(index)
+	if err != nil {
+		log.Println(logError + "Failed to ensure \"index\" on collection: " + db.RepoColl + "!")
+		return err
+	}
+
+	// Postpone closing connection until we return
+	defer session.Close()
+
+	log.Println(logInfo + "Initializing successfull!")
+	// Nothing bad happened!
+	return nil
+}
+
+//add adds rm to db if it is not already in it.
+func (db *MongoDB) add(rm *RepoModel) error {
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		log.Fatalln(logError+"Can't connect to database: ", err)
+	}
+	defer session.Close()
+
+	exstRepo, err := db.findRepoByURI(rm.URI)
+
+	if err != nil {
+		return err
+	}
+
+	if exstRepo.ID.Valid() {
+		return errors.New("Already exists")
+	}
+
+	rm.ID = bson.NewObjectId()
+
+	return session.DB(db.DatabaseName).C(db.RepoColl).Insert(rm)
+}
+
+// findRepoByURI takes the repo with field uri as given uri.
+// It returns empty repo if it is not in db.
+func (db *MongoDB) findRepoByURI(uri string) (repo RepoModel, err error) {
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		log.Fatalln(logError+"Can't connect to database: ", err)
+	}
+	defer session.Close()
+
+	// Retrun empty repo with error if error is not "Not found"
+	if err = session.DB(db.DatabaseName).C(db.RepoColl).Find(bson.M{"uri": uri}).One(&repo); err != nil && err.Error() != "not found" {
+		return RepoModel{}, err
+	}
+
+	return repo, nil
+
+}
+
+// FindRepoByID takes the repo with field id as given id.
+// It returns empty repo if it is not in db.
+func (db *MongoDB) FindRepoByID(id string) (repo RepoModel, err error) {
+	session, err := mgo.Dial(db.DatabaseURL)
+	if err != nil {
+		log.Fatalln(logError+"Can't connect to database: ", err)
+	}
+	defer session.Close()
+
+	if !bson.IsObjectIdHex(id) {
+		err = errors.New("Invalid id")
+		log.Println(logError+"Id received is incorrect: ", err)
+		return RepoModel{}, err
+	}
+
+	keyID := bson.ObjectIdHex(id)
+
+	// Retrun empty repo with error if error is not "Not found"
+	if err = session.DB(db.DatabaseName).C(db.RepoColl).Find(bson.M{"_id": keyID}).One(&repo); err != nil && err.Error() != "not found" {
+		return RepoModel{}, err
+	}
+
+	return repo, nil
+
+}
