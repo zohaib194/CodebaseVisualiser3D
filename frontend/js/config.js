368f2e3f11be1a5c461091ff11fc3025a677c025
Xillez <kentwh@stud.ntnu.no>
2019-02-21 09:34:14 +0100

    COD-27 Removed html Content-Security-Policy meta tag and moved config into separate folder

--- frontend/js/config.js
+++
@@ -1,8 +0,0 @@
-const host_ip = "localhost";
-const host_port = "80";
-const api_ip = "localhost";
-const api_port = "8080";
-const api_servername = "apiServer";
-
-// Dont touch underneath this comment
-document.domain = "glworkshop.com";
