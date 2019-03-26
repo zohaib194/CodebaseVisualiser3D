03c70826f1179f086d26cb3a156d44ad38ea07f0
Xillez <kentwh@stud.ntnu.no>
2019-03-26 10:39:26 +0100

    COD-72 Added missing getter, setter and addition functions. Cleaned up ordring of these functions and performed automatic formatting.

--- backend/parser/src/main/java/me/codvis3D/ast/model/FileModel.java
+++ backend/parser/src/main/java/me/codvis3D/ast/model/FileModel.java
@@ -10,20 +10,19 @@ import org.json.JSONObject;
 /**
  * Class for file model.
  */
-public class FileModel extends Model{
+public class FileModel extends Model {
 	private String fileName;
 	private List<FunctionModel> functions;
 	private List<NamespaceModel> namespaces;
 	private List<UsingNamespaceModel> usingNamespaces;
 	private List<VariableModel> variables;
 
-
 	/**
 	 * Constructs the object, setting the filename.
 	 *
-	 * @param      fileName  The file name
+	 * @param fileName The file name
 	 */
-	FileModel(String fileName){
+	FileModel(String fileName) {
 		this.fileName = fileName;
 		this.functions = new ArrayList<>();
 		this.namespaces = new ArrayList<>();
@@ -32,115 +31,140 @@ public class FileModel extends Model{
 	}
 
 	/**
-	 * Adds a function.
-	 *
-	 * @param      function  The function
-	 */
-	public void addFunction(FunctionModel function){
-		this.functions.add(function);
-	}
-
-	/**
-	 * Adds a namespace.
-	 *
-	 * @param      namespace  The namespace
-	 */
-	public void addNamespace(NamespaceModel namespace){
-		this.namespaces.add(namespace);
-	}
-
-	/**
-	 * Adds an using namespace.
-	 *
-	 * @param      namespace  The namespace
-	 */
-	public void addUsingNamespace(UsingNamespaceModel namespace){
-		this.usingNamespaces.add(namespace);
-	}
-
-	/**
-	 * Adds a variable.
-	 *
-	 * @param      variable  The variable
-	 */
-	public void addVariable(VariableModel variable){
-		this.variables.add(variable);
-	}
-
-	/**
-	 * Sets the variables.
-	 *
-	 * @param      variables  The variables
-	 */
-	public void setVariables(List<VariableModel> variables){
-		this.variables = variables;
-	}
-
-	/**
-	 * Sets the functions.
-	 *
-	 * @param      functions  The functions
-	 */
-	public void setFunctions(List<FunctionModel> functions){
-		this.functions = functions;
-	}
-
-
-	/**
 	 * Gets the filename.
 	 *
-	 * @return     The filename.
+	 * @return The filename.
 	 */
-	public String getFilename(){
+	public String getFilename() {
 		return this.fileName;
 	}
 
 	/**
 	 * Gets the functions.
 	 *
-	 * @return     The functions.
+	 * @return The functions.
 	 */
-	public List<FunctionModel> getFunctions(){
+	public List<FunctionModel> getFunctions() {
 		return this.functions;
 	}
-
 
 	/**
 	 * Gets the namespaces.
 	 *
-	 * @return     The namespaces.
+	 * @return The namespaces.
 	 */
-	public List<NamespaceModel> getNamespaces(){
+	public List<NamespaceModel> getNamespaces() {
 		return this.namespaces;
 	}
 
 	/**
 	 * Gets the using namespaces.
 	 *
-	 * @return     The used namespaces.
+	 * @return The used namespaces.
 	 */
-	public List<UsingNamespaceModel> getUsingNamespaces(){
+	public List<UsingNamespaceModel> getUsingNamespaces() {
 		return this.usingNamespaces;
+	}
+
+	/**
+	 * Gets the using variables.
+	 *
+	 * @return The used variables.
+	 */
+	public List<VariableModel> getVariables() {
+		return this.variables;
+	}
+
+	/**
+	 * Sets the functions.
+	 *
+	 * @param functions The functions
+	 */
+	public void setFunctions(List<FunctionModel> functions) {
+		this.functions = functions;
+	}
+
+	/**
+	 * Sets the namespaces.
+	 *
+	 * @param namespaces The namespaces
+	 */
+	public void setNamespaces(List<NamespaceModel> namespaces) {
+		this.namespaces = namespaces;
+	}
+
+	/**
+	 * Sets the usingNamespaces.
+	 *
+	 * @param usingNamespaces The usingNamespaces
+	 */
+	public void setUsingNamespaces(List<NamespaceModel> usingNamespaces) {
+		this.usingNamespaces = usingNamespaces;
+	}
+
+	/**
+	 * Sets the variables.
+	 *
+	 * @param variables The variables
+	 */
+	public void setVariables(List<VariableModel> variables) {
+		this.variables = variables;
+	}
+
+	/**
+	 * Adds a function.
+	 *
+	 * @param function The function
+	 */
+	public void addFunction(FunctionModel function) {
+		this.functions.add(function);
+	}
+
+	/**
+	 * Adds a namespace.
+	 *
+	 * @param namespace The namespace
+	 */
+	public void addNamespace(NamespaceModel namespace) {
+		this.namespaces.add(namespace);
+	}
+
+	/**
+	 * Adds an using namespace.
+	 *
+	 * @param namespace The namespace
+	 */
+	public void addUsingNamespace(UsingNamespaceModel usingNamespace) {
+		this.usingNamespaces.add(usingNamespace);
+	}
+
+	/**
+	 * Adds a variable.
+	 *
+	 * @param variable The variable
+	 */
+	public void addVariable(VariableModel variable) {
+		this.variables.add(variable);
 	}
 
 	/**
 	 * Adds the data in model.
 	 *
-	 * @param      data  The data
+	 * @param data The data
 	 */
 	@Override
-	protected <T> void addDataInModel(T data){
+	protected <T> void addDataInModel(T data) {
 
 		if (data instanceof FunctionModel) {
 			this.addFunction((FunctionModel) data);
 
-		} else if (data instanceof  NamespaceModel) {
+		} else if (data instanceof NamespaceModel) {
 			this.addNamespace((NamespaceModel) data);
 
 		} else if (data instanceof UsingNamespaceModel) {
 			this.addUsingNamespace((UsingNamespaceModel) data);
 
-		} else if (data instanceof VariableModel){
+		} else if (data instanceof VariableModel) {
 			this.addVariable((VariableModel) data);
 
 		} else {
@@ -154,10 +178,10 @@ public class FileModel extends Model{
 	/**
 	 * Gets the parsed code as a JSONObject.
 	 *
-	 * @return     The parsed code.
+	 * @return The parsed code.
 	 */
 	@Override
-	public JSONObject getParsedCode(){
+	public JSONObject getParsedCode() {
 		JSONObject parsedCode = new JSONObject();
 
 		parsedCode.put("file_name", this.fileName);
