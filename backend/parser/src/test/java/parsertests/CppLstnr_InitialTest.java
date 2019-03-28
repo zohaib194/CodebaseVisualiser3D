709a1ecfcfb594c50d5469c202fb90d9c2f47af5
Xillez <kentwh@stud.ntnu.no>
2019-03-28 11:22:48 +0100

    COD-71 Added Test file to Initial cpp parser

---
+++ backend/parser/src/test/java/parsertests/CppLstnr_InitialTest.java
@@ -0,0 +1,17 @@
+package me.codvis.ast;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class CppLstnr_InitialTest {
+
+	@Test
+	@Disabled
+	public void test() {
+
+	}
+}
